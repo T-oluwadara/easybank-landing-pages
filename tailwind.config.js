85596f786c1f041f37005e9de6be9b45c44dbf5e
T-oluwadara <ajimotioluwadara@gmail.com>
2022-08-04 19:42:47 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      backgroundImage: {
+        heroImage: "url('/images/bg-intro-mobile.svg')"
+      },
+      colors: {
+        darkBlue: 'hsl(233, 26%, 24%)',
+        limeGreen: 'hsl(136, 65%, 51%)',
+        brightCyan: 'hsl(192, 70%, 51%)',
+        grayishBlue: 'hsl(233, 8%, 62%)',
+        lightGrayishBlue: 'hsl(220, 16%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        White: 'hsl(0, 0%, 100%)'
+      },
+    },
+  },
+  plugins: [],
+}
